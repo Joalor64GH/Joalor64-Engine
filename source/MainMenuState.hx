a7539507ab2611cac914189d2a261a99fe8b9768
Joalor64 <rhgarcon@gmail.com>
2022-04-17 14:39:00 -0400

    adding back achievements

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -36,6 +36,7 @@ class MainMenuState extends MusicBeatState
 		'story_mode',
 		'freeplay',
 		#if MODS_ALLOWED 'mods', #end
+		#if ACHIEVEMENTS_ALLOWED 'awards', #end
 		'credits',
 		#if !switch 'donate', #end
 		'options'
