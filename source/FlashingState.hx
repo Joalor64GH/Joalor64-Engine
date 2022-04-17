cc49b4345808b7578edabc89b75e1a24bd312354
Joalor64 <rhgarcon@gmail.com>
2022-04-17 16:53:33 -0400

    new thing lol

--- source/FlashingState.hx
+++ source/FlashingState.hx
@@ -24,11 +24,11 @@ class FlashingState extends MusicBeatState
 		add(bg);
 
 		warnText = new FlxText(0, 0, FlxG.width,
-			"Hey, watch out!\n
-			This Mod contains some flashing lights!\n
-			Press ENTER to disable them now or go to Options Menu.\n
-			Press ESCAPE to ignore this message.\n
-			You've been warned!",
+			"Hey, thanks for downloading this!\n
+			However, there are some flashing lights that could cause seizures!\n
+			Press ENTER to disable them now or do it in the Options Menu.\n
+			Or just press ESCAPE to continue.\n
+			Funk all the way!",
 			32);
 		warnText.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
 		warnText.screenCenter(Y);
