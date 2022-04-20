caa118328d73bb021fa5f7d118ed2e26953e2eda
Joalor64 <rhgarcon@gmail.com>
2022-04-20 15:59:41 -0400

    3d notes scripts

---
+++ example_mods/scripts/3DNotes2.lua
@@ -0,0 +1,14 @@
+function onUpdate(elapsed)
+	if getProperty('health') > 0.06 then
+		if dadName == 'jambi' then --replace the name for your character name
+			for i=0,4,1 do
+				setPropertyFromGroup('opponentStrums', i, 'texture', 'NOTE_assets_3D')
+			end
+			for i = 0, getProperty('unspawnNotes.length')-1 do
+				if not getPropertyFromGroup('unspawnNotes', i, 'mustPress') then
+					setPropertyFromGroup('unspawnNotes', i, 'texture', 'NOTE_assets_3D'); --Change texture
+				end
+			end
+		end
+	end
+end
