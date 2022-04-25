d79a1c2e4c4f58b572c895391b0e04937f81fd62
Joalor64 <rhgarcon@gmail.com>
2022-04-25 18:36:18 -0400

    latin alphabet support

--- source/Alphabet.hx
+++ source/Alphabet.hx
@@ -372,11 +372,11 @@ class Alphabet extends FlxSpriteGroup
 
 class AlphaCharacter extends FlxSprite
 {
-	public static var alphabet:String = "abcdefghijklmnopqrstuvwxyz";
+	public static var alphabet:String = "abcdefghijklmnopqrstuvwxyzàáãâåèéêëìíîïòóôöùúûüñçþæğş";
 
 	public static var numbers:String = "1234567890";
 
-	public static var symbols:String = "|~#$%()*+-:;<=>@[]^_.,'!?";
+	public static var symbols:String = "|~#$%()[]*+-:;<=>@^_.,'!?/\\";
 
 	public var row:Int = 0;
 
@@ -424,6 +424,10 @@ class AlphaCharacter extends FlxSprite
 				animation.addByPrefix(letter, 'bold (', 24);
 			case ")":
 				animation.addByPrefix(letter, 'bold )', 24);
+			case "$":
+				animation.addByPrefix(letter, '$', 24);
+			case '^':
+				animation.addByPrefix(letter, '^ bold', 24);
 			default:
 				animation.addByPrefix(letter, 'bold ' + letter, 24);
 		}
@@ -495,6 +499,8 @@ class AlphaCharacter extends FlxSprite
 				animation.addByPrefix(letter, 'exclamation point', 24);
 			case ",":
 				animation.addByPrefix(letter, 'comma', 24);
+			case '/':
+				animation.addByPrefix(letter, 'forward slash', 24);
 			default:
 				animation.addByPrefix(letter, letter, 24);
 		}
