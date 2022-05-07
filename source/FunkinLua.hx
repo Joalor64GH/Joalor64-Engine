72a933bfc4b3f152d59d63f93949ba1c073ea151
Joalor64 <rhgarcon@gmail.com>
2022-05-07 18:09:48 -0400

    whatever this is

--- source/FunkinLua.hx
+++ source/FunkinLua.hx
@@ -109,9 +109,6 @@ class FunkinLua {
 		set('weekRaw', PlayState.storyWeek);
 		set('week', WeekData.weeksList[PlayState.storyWeek]);
 		set('seenCutscene', PlayState.seenCutscene);
-		
-		// Block require and os, Should probably have a proper function but this should be good enough for now until someone smarter comes along and recreates a safe version of the OS library
-		set('require', false);
 
 		// Camera poo
 		set('cameraX', 0);
