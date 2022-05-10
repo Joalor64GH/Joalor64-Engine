57b37c7ec73fabef68168da11cb02f4ed75f1be0
Joalor64 <rhgarcon@gmail.com>
2022-05-10 08:47:18 -0400

    dialogue fix for mac

--- source/FunkinLua.hx
+++ source/FunkinLua.hx
@@ -44,9 +44,14 @@ import Discord;
 using StringTools;
 
 class FunkinLua {
+	#if mac
+	public static var Function_Stop:Dynamic = "Function_Stop";
+	public static var Function_Continue:Dynamic = "Function_Continue";
+	#else
 	public static var Function_Stop:Dynamic = 1;
-	public static var Function_Continue:Dynamic = 0;
-
+	public static var Function_Continue:Dynamic = 0;	
+	#end
+	
 	#if LUA_ALLOWED
 	public var lua:State = null;
 	#end
@@ -87,8 +92,13 @@ class FunkinLua {
 		#end
 
 		// Lua shit
+		#if mac
+		set('Function_Stop', "Function_Stop");
+		set('Function_Continue', "Function_Continue");
+		#else
 		set('Function_Stop', Function_Stop);
 		set('Function_Continue', Function_Continue);
+		#end
 		set('luaDebugMode', false);
 		set('luaDeprecatedWarnings', true);
 		set('inChartEditor', false);
