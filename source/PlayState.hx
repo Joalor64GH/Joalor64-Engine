fc6dbdb3d966965aa8a377cfe516fb79fd0dfa26
Joalor64 <rhgarcon@gmail.com>
2022-05-13 19:20:43 -0400

    restore old bf icon easter egg

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -774,6 +774,14 @@ class PlayState extends MusicBeatState
 			}
 		}
 
+	        if (FlxG.keys.justPressed.NINE)
+		{
+			if (iconP1.animation.curAnim.name == 'bf-old')
+				iconP1.animation.play(SONG.player1);
+			else
+				iconP1.animation.play('bf-old');
+		}
+	
 		if(doPush) 
 			luaArray.push(new FunkinLua(luaFile));
 		#end
