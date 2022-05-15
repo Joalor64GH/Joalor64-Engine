9bd6afee82e288ba8c4e74170d70ffde4588de40
Joalor64 <rhgarcon@gmail.com>
2022-05-15 07:24:29 -0400

    placeholder week or something

--- source/WeekData.hx
+++ source/WeekData.hx
@@ -52,13 +52,13 @@ class WeekData {
 
 	public static function createWeekFile():WeekFile {
 		var weekFile:WeekFile = {
-			songs: [["Bopeebo", "dad", [146, 113, 253]], ["Fresh", "dad", [146, 113, 253]], ["Dad Battle", "dad", [146, 113, 253]]],
+			songs: [["Song 1", "face", [135, 131, 158]], ["Song 2", "face", [135, 131, 158]], ["Song 3", "face", [135, 131, 158]]],
 			weekCharacters: ['dad', 'bf', 'gf'],
 			weekBackground: 'stage',
 			weekBefore: 'tutorial',
 			storyName: 'Your New Week',
 			weekName: 'Custom Week',
-			freeplayColor: [146, 113, 253],
+			freeplayColor: [135, 131, 158],
 			startUnlocked: true,
 			hiddenUntilUnlocked: false,
 			hideStoryMode: false,
