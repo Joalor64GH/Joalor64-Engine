95d64c23790f33cbd06955bcad11aa31c2af03d4
Joalor64 <rhgarcon@gmail.com>
2022-05-30 17:10:59 -0400

    frickin forget it

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -839,19 +839,6 @@ class PlayState extends MusicBeatState
 		boyfriendGroup.add(boyfriend);
 		startCharacterLua(boyfriend.curCharacter);
 		
-	if (PlayState.isPixelStage)
-			
-		        comboUI = new ComboPopup(boyfriend.x - 250, boyfriend.y - 75,	[Paths.image("shared/images/comboBreak"), 403, 163, true], 
-																			[Paths.image("shared/images/comboBreak"), 348, 211, true]);
-		}
-		else{
-		}
-			comboUI = new ComboPopup(boyfriend.x - 250, boyfriend.y - 75, 	[Paths.image("shared/images/pixelUI/comboBreak-pixel"), 51, 20, false], 
-																			[Paths.image("shared/images/pixelUI/comboBreak-pixel"), 53, 32, false], 
-																			[daPixelZoom * 0.7, daPixelZoom * 0.8, daPixelZoom * 0.7]);
-			comboUI.numberPosition[0] -= 120;
-		}
-	
 		var camPos:FlxPoint = new FlxPoint(girlfriendCameraOffset[0], girlfriendCameraOffset[1]);
 		if(gf != null)
 		{
@@ -4079,7 +4066,6 @@ class PlayState extends MusicBeatState
 			if (combo > 5 && gf != null && gf.animOffsets.exists('sad'))
 			{
 				gf.playAnim('sad');
-				comboUI.breakPopup();
 			}
 			combo = 0;
 
