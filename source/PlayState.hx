6d38f4ae8760fa052b65bfa81cc536a3f5a5290c
Joalor64 <rhgarcon@gmail.com>
2022-05-30 17:05:16 -0400

    did i do it??

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -845,6 +845,7 @@ class PlayState extends MusicBeatState
 																			[Paths.image("shared/images/comboBreak"), 348, 211, true]);
 		}
 		else{
+		}
 			comboUI = new ComboPopup(boyfriend.x - 250, boyfriend.y - 75, 	[Paths.image("shared/images/pixelUI/comboBreak-pixel"), 51, 20, false], 
 																			[Paths.image("shared/images/pixelUI/comboBreak-pixel"), 53, 32, false], 
 																			[daPixelZoom * 0.7, daPixelZoom * 0.8, daPixelZoom * 0.7]);
