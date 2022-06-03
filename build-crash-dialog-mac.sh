217850d420cf6172ee7b01e94e549b67b2735ae5
Joalor64 <rhgarcon@gmail.com>
2022-06-03 19:03:13 -0400

    idk anymore at this point

--- build-crash-dialog-mac.sh
+++ build-crash-dialog-mac.sh
@@ -1,5 +1,5 @@
 cd crash-dialog
 echo "Building crash dialog..."
 lime build mac
-cp build/openfl/mac/bin/CrashDialog ../export/release/mac/bin/CrashDialog
+cp build/openfl/mac/bin/CrashDialog ../export/release/mac/bin/J64E-CrashDialog
 cd ..
