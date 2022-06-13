e6f3c672496ce3ef717786c2608ddc41fe9fc3f7
Joalor64 <rhgarcon@gmail.com>
2022-06-13 17:57:06 -0400

    i forgot dammit

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -74,7 +74,7 @@ class MainMenuState extends MusicBeatState
 
 		#if desktop
 		// Updating Discord Rich Presence
-		DiscordClient.changePresence("In the Menus", null);
+		DiscordClient.changePresence("Main Menu", null);
 		#end
 		debugKeys = ClientPrefs.copyKey(ClientPrefs.keyBinds.get('debug_1'));
                 modShortcutKeys = ClientPrefs.copyKey(ClientPrefs.keyBinds.get('debug_2'));
