b09202f09402de74cb23a6e953fde34405117a00
Joalor64 <rhgarcon@gmail.com>
2022-07-16 20:37:08 -0400

    debug stuff

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -241,7 +241,7 @@ class MainMenuState extends MusicBeatState
 
 		FlxG.camera.follow(camFollowPos, null, 1);
 
-		var versionShit:FlxText = new FlxText(12, FlxG.height - 64, 0, "Joalor64 Engine v" + joalor64EngineVersion, 12);
+		var versionShit:FlxText = new FlxText(12, FlxG.height - 64, 0, "Joalor64 Engine v" + joalor64EngineVersion #if debug + " DEBUG BUILD" #end, 12);
 		versionShit.scrollFactor.set();
 		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		add(versionShit);
