f1c57e26f14c475ae9ffa2b3c259acb120018014
Joalor64 <rhgarcon@gmail.com>
2022-08-06 09:41:42 -0400

    script handler

---
+++ source/ScriptHandler.hx
@@ -0,0 +1,31 @@
+package;
+
+import flixel;
+import flixel.math;
+import flixel.system;
+import flixel.tweens;
+import flixel.util;
+
+class ScriptHandler extends SScript
+{
+    public function new(file:String, ?preset:Bool = true)
+    {
+        super(file, preset);
+    }
+
+    override public function preset():Void
+    {
+        super.preset();
+
+		interp.variables.set('FlxG', FlxG);
+		interp.variables.set('FlxBasic', FlxBasic);
+		interp.variables.set('FlxObject', FlxObject);
+		interp.variables.set('FlxCamera', FlxCamera);
+		interp.variables.set('FlxSprite', FlxSprite);
+		interp.variables.set('FlxTimer', FlxTimer);
+		interp.variables.set('FlxTween', FlxTween);
+		interp.variables.set('FlxEase', FlxEase);
+		interp.variables.set('FlxMath', FlxMath);
+		interp.variables.set('FlxSound', FlxSound);
+    }
+}
