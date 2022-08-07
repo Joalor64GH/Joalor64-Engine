b068ea3247ab52836141446428c9dc7653aa425e
Joalor64 <rhgarcon@gmail.com>
2022-08-07 12:12:13 -0400

    hx plugins code from modding plus

---
+++ source/FNFAssets.hx
@@ -0,0 +1,333 @@
+package;
+
+// NO NOT WEEK 7 THAT CAN FUCK OFF
+// A helper class to make supporting web easier
+#if sys
+import sys.FileSystem;
+import sys.io.File;
+#end
+import openfl.utils.Assets;
+import lime.utils.Assets as LimeAssets;
+import openfl.display.BitmapData;
+import openfl.media.Sound;
+import haxe.io.Path;
+import flixel.FlxG;
+import flash.net.FileReference;
+import flash.events.Event;
+import openfl.events.IOErrorEvent;
+import haxe.io.Bytes;
+import openfl.utils.AssetType;
+using StringTools;
+enum Extensions {
+	None;
+	Json;
+	Hscript;
+}
+/**
+ * Assets reader and writer
+ */
+class FNFAssets {
+    public static var _file:FileReference;
+    /**
+     * Get text content of a file. 
+     * @param id Path to file.
+     * @return String The file content. 
+     */
+    public static function getText(id:String):String {
+        #if sys
+            // if there a library strip it out..
+            // future proofing ftw
+			if (!isInScope(id))
+				throw "Tried to access a file that is out of scope.";
+			var path = Assets.exists(id) ? Assets.getPath(id) : null;
+            if (path == null)
+                path = id;
+			else
+				return Assets.getText(id);
+			try {
+			return File.getContent(path);
+			} catch (e:Any) {
+				throw 'File $path doesn\'t exist or cannot be read.';
+			}
+
+        #else
+            // no need to strip it out... 
+            // assets handles it
+            return Assets.getText(id);
+        #end
+    }
+	/**
+	 * Get json, auto checking extensions.
+	 * @param id Path without extension
+	 */
+	static public function getJson(id:String):Null<String> {
+		return getAmbigAsset([id], CoolUtil.JSON_EXT, AssetType.TEXT);
+	}
+	static public function getHscript(id:String):Null<String> {
+		return getAmbigAsset([id], CoolUtil.HSCRIPT_EXT,AssetType.TEXT);
+	}
+	/**
+	 * A safer way to get assets. Checks if the first asset exists and if not ALWAYS uses 2nd asset.
+	 * This means backupID should be guarenteed to exist. 
+	 * @param id The id wanting to be read
+	 * @param backupID the id to read if wanted one does not exist
+	 * @param type Type of the file 
+	 * @return Dynamic The file, in the type requested.
+	 */
+	public static function getAssetWithBackup(id:String, backupID:String, type:AssetType):Dynamic {
+		// backup id should always exist
+		if (FNFAssets.exists(id)) {
+			return FNFAssets.getAsset(id, type);
+		}
+		return FNFAssets.getAsset(backupID, type);
+	} 
+	/**
+	 * Generic way to get assets
+	 * @param id The path/id of the item.
+	 * @param type The type of the object.
+	 * @return Dynamic The file read in the type requested. 
+	 */
+	public static function getAsset(id:String, type:AssetType):Dynamic {
+		switch (type) {
+			case TEXT:
+				return FNFAssets.getText(id);
+			case BINARY:
+				return FNFAssets.getBytes(id);
+			case MUSIC | SOUND:
+				return FNFAssets.getSound(id);
+			case IMAGE:
+				return FNFAssets.getBitmapData(id);
+			default:
+				throw "Unsure of how to get type " + type;
+		}
+	}
+	/**
+	 * Get an asset that could have multiple names/extensions.
+	 * @param id Array of path names
+	 * @param ext Array of string for extension.
+	 * @param type Type of asset.
+	 * @return Dynamic WARNING, if it doesn't find anything it will return null.
+	 */
+	public static function getAmbigAsset(id:Array<String>, ext:Array<String>, type:AssetType):Dynamic {
+		for (path in id)
+		{
+			for (ex in ext)
+			{
+				if (exists(path + '.' + ex))
+				{
+					return getAsset(path + '.' + ex, type);
+				}
+			}
+		}
+		return null;
+	}
+	public static function existsAmbig(id:Array<String>, extension:Array<String>):String {
+		for (path in id)
+		{
+			for (ext in extension)
+			{
+				if (exists(path + '.' + ext))
+					return path + '.' + ext;
+			}
+		}
+		return '';
+	}
+	public static function getBytes(id:String):Bytes
+	{
+		#if sys
+		// if there a library strip it out..
+		// future proofing ftw
+			if (!isInScope(id))
+				throw "Tried to access a file that is out of scope.";
+			var path = Assets.exists(id) ? Assets.getPath(id) : null;
+			if (path == null)
+				path = id;
+			else
+				return Assets.getBytes(id);
+			try {
+			return File.getBytes(path);
+			} catch (e:Any) {
+			throw 'File $path doesn\'t exist or cannot be read.';
+			}
+
+		#else
+		// no need to strip it out...
+		// assets handles it
+		return LimeAssets.getBytes(id);
+		#end
+	}
+    /**
+     * Check if the file exists.
+     * @param id The file to check
+	 * @param ext Extension to auto check against. For fine tune control use "existsAmbig"
+     * @return Bool If file exists, true.
+     */
+    static public function exists(id:String, ?ext:Extensions):Bool {
+		switch (ext) {
+			case Json: 
+				return existsAmbig([id], CoolUtil.JSON_EXT) != '';
+			case Hscript: 
+				return existsAmbig([id], CoolUtil.HSCRIPT_EXT) != '';
+			default: 
+				if (!isInScope(id))
+					return false;
+				#if sys
+				var path = Assets.exists(id) ? Assets.getPath(id) : null;
+				if (path == null)
+					path = id;
+				else
+					// if it _does_ exist then yeah of course  it works
+					return true;
+				return FileSystem.exists(path);
+				#else
+				return Assets.exists(id);
+				#end
+		}
+    }
+	/**
+	 * Check if a file is in the cwd. Used to prevent sussy bakas from being sussy
+	 * @param id 
+	 */
+	public static function isInScope(id:String) {
+		#if sys
+		if (Assets.exists(id))
+			return true;
+		// If path isn't within cwd return false
+		if (!Path.normalize(FileSystem.absolutePath(id)).contains(Path.normalize(Main.cwd)))
+			return false;
+		#end
+		return true;
+	}
+    /**
+     * Get bitmap data of a file.
+     * @param id Path of file
+     * @param useCache Whether to reuse assets if file was already requested. Only works on non-dynamically loaded assets.
+     * @return BitmapData the data of the file.
+     */
+    public static function getBitmapData(id:String, ?useCache:Bool=true):BitmapData {
+        #if sys
+			if (!isInScope(id))
+				throw "Tried to access a file that is out of scope.";
+            // idk if this works lol
+			var path = Assets.exists(id) ? Assets.getPath(id) : null;
+            if (path == null)
+                path = id;
+			else return Assets.getBitmapData(id, useCache);
+			try {
+			return BitmapData.fromFile(path);
+			} catch (e:Any) {
+			throw 'File $path doesn\'t exist or cannot be read.';
+			}
+
+        #else
+            return Assets.getBitmapData(id, useCache);
+        #end
+    }
+    /**
+     * Get sound from file.
+     * @param id Path of file
+     * @param useCache whether to reuse assets if file was already requested. Only works on non-dynamically loaded files.
+	 * @return Sound The sound file.
+     */
+    public static function getSound(id:String, ?useCache:Bool=true):Sound {
+        #if sys
+			if (!isInScope(id))
+				throw "Tried to access a file that is out of scope.";
+			var path = Assets.exists(id) ? Assets.getPath(id) : null;
+            if (path == null)
+                path = id;
+			else
+				// prefer using assets as it uses a cache??
+				return Assets.getSound(id, useCache);
+		try
+		{
+			return Sound.fromFile(path);
+		}
+		catch (e:Any)
+		{
+			throw 'File $path doesn\'t exist or cannot be read.';
+		}
+        #else
+            return Assets.getSound(id, useCache);
+        #end
+    }
+    /**
+     * Save content to a file. 
+     * @param id File to save to. 
+     * @param data Data to save.
+     */
+    public static function saveContent(id:String, data:String):Void {
+        #if sys
+			if (!isInScope(id))
+				throw "Tried to access a file that is out of scope.";
+			try {
+				File.saveContent(id, data);
+			}	catch(e:Any) {
+				throw "Couldn't save to "+ id +". Is it in use?";
+			}
+
+        #else
+            askToSave(id, data);
+        #end
+    }
+	/**
+	 * Save bytes to a file.
+	 * @param id File to save to 
+	 * @param data Bytes to save. 
+	 */
+	public static function saveBytes(id:String, data:Bytes)
+	{
+		#if sys
+		if (!isInScope(id))
+			throw "Tried to access a file that is out of scope.";
+		try
+		{
+			File.saveBytes(id, data);
+		}
+		catch (e:Any)
+		{
+			throw "Couldn't save to " + id + ". Is it in use?";
+		}
+		#else
+		askToSave(id, data);
+		#end
+	}
+	/**
+	 * Ask the user to pick a path to save to. Used on web when other save functions are called.
+	 * @param id Path to save to.
+	 * @param data Data. Can be anything. 
+	 */
+	public static function askToSave(id:String, data:Dynamic)
+	{
+		_file = new FileReference();
+
+		_file.addEventListener(Event.COMPLETE, onSaveComplete);
+		_file.addEventListener(Event.CANCEL, onSaveCancel);
+		_file.addEventListener(IOErrorEvent.IO_ERROR, onSaveError);
+		var idSus = Path.withoutDirectory(id);
+		_file.save(data, idSus);
+	}
+	static function onSaveComplete(_):Void
+	{
+		_file.removeEventListener(Event.COMPLETE, onSaveComplete);
+		_file.removeEventListener(Event.CANCEL, onSaveCancel);
+		_file.removeEventListener(IOErrorEvent.IO_ERROR, onSaveError);
+		_file = null;
+		FlxG.log.notice("Successfully saved LEVEL DATA.");
+	};
+	static function onSaveCancel(_):Void
+	{
+		_file.removeEventListener(Event.COMPLETE, onSaveComplete);
+		_file.removeEventListener(Event.CANCEL, onSaveCancel);
+		_file.removeEventListener(IOErrorEvent.IO_ERROR, onSaveError);
+		_file = null;
+	};
+	static function onSaveError(_):Void
+	{
+		_file.removeEventListener(Event.COMPLETE, onSaveComplete);
+		_file.removeEventListener(Event.CANCEL, onSaveCancel);
+		_file.removeEventListener(IOErrorEvent.IO_ERROR, onSaveError);
+		_file = null;
+		FlxG.log.error("Problem saving Level data");
+	}
+} 
---
+++ source/plugins/ExamplePlugin.hx
@@ -0,0 +1,223 @@
+package plugins;
+
+import lime.ui.Window;
+import flixel.system.FlxSound;
+import flixel.FlxSprite;
+import flixel.FlxCamera;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import Song.SwagSong;
+
+/**
+ * documentation for hscripts : )
+ * Note that functions marked as "Dynamic" mean _you_ should define them
+ */
+ @:keep
+@:publicFields
+class ExamplePlugin {
+    /**
+     * Component of layer, behind GF. Can be binary OR'd `|` with other layers to combine them
+     */
+    public final BEHIND_GF:Int = DisplayLayer.BEHIND_GF;
+	/**
+	 * Component of layer, behind BF. Can be binary OR'd `|` with other layers to combine them
+	 */
+    public final BEHIND_BF:Int = DisplayLayer.BEHIND_BF;
+	/**
+	 * Component of layer, behind Enemy. Can be binary OR'd `|` with other layers to combine them
+	 */
+    public final BEHIND_DAD:Int = DisplayLayer.BEHIND_DAD;
+	/**
+	 * Component of layer, behind ALl. Can be binary OR'd `|` with other layers to combine them
+	 */
+    public final BEHIND_ALL:Int = DisplayLayer.BEHIND_ALL;
+	/**
+	 * Component of layer, behind None. Can be binary OR'd `|` with other layers to combine them
+	 */
+    public final BEHIND_NONE:Int = 0;
+    /**
+     * A number representing the difficulty. Not very useful, don't use
+     */
+    public var difficulty:Int;
+    /**
+     * Bpm of current song. 
+     */
+     @:deprecated('Use Conductor.bpm instead')
+    public var bpm:Float;
+    /**
+     * Data of song. See swag song.
+     */
+    public var songData:SwagSong;
+    /**
+     * Name of current song.
+     */
+    public var curSong:String;
+    /**
+     * Cur beat
+     */
+    public var curBeat:Int;
+    /**
+     * Current step which is 1/4 of a beat
+     */
+    public var curStep:Int;
+    /**
+     * Camera used for UI elements
+     */
+    public var camHUD:FlxCamera;
+    /**
+     * Whether to only display notes and note strums.
+     */
+    public var showOnlyStrums:Bool;
+    /**
+     * Right side strums
+     */
+    public var playerStrums:FlxTypedGroup<FlxSprite>;
+    /**
+     * Left side strums
+     */
+    public var enemyStrums:FlxTypedGroup<FlxSprite>; 
+    /**
+     * Whether current note must be hit
+     */
+    public var mustHit:Bool;
+    /**
+     * Y Position of strumline
+     */
+    public var strumLineY:Int;
+    /**
+     * Path to directory that uasually contains files relating to your
+     * hscript
+     */
+    public var hscriptPath:String;
+    /**
+     * Player 1 character
+     */
+    public var boyfriend:Character;
+    /**
+     * Gf Character
+     */
+    public var gf:Character;
+    /**
+     * player 2 character
+     */
+    public var dad:Character;
+    /**
+     * sound of vocals
+     */
+    public var vocals:FlxSound;
+    /**
+     * How fast gf goes. Higher numbers are slower.
+     */
+    public var gfSpeed:Float;
+    /**
+     * Tween the cam in
+     */
+    public function tweenCamIn():Void {}
+    /**
+     * True if in cutscene. Stops a lot of processes.
+     */
+    public var isInCutscene:Bool;
+    /**
+     * If cam is zooming in. 
+     */
+    public var camZooming:Bool;
+    /**
+     * Add a sprite
+     * @param sprite the sprite
+     * @param layer the layer, which is combos of the BEHIND things
+     */
+    public function addSprite(sprite:FlxSprite, layer:Int):Void {}
+    /**
+     * Set default zoom which is what is used by camZooming
+     * @param zoom 
+     */
+    public function setDefaultZoom(zoom:Float):Void {}
+    /**
+     * Remove sprite
+     * @param sprite Sprite to remove
+     */
+    public function removeSprite(sprite:FlxSprite):Void {}
+    /**
+     * Get an actor
+     * @param name Name of actor. 
+     * @return Dynamic The actor
+     */
+    public function getHaxeActor(name:String):Dynamic {throw 'sussy baka';}
+    /**
+     * current health
+     */
+    public var health:Float;
+    /**
+     * P1's Icon
+     */
+    public var iconP1:HealthIcon;
+    /**
+     * P2's Icon
+     */
+    public var iconP2:HealthIcon;
+    /**
+     * The current playstate
+     */
+    public var currentPlayState:PlayState;
+    /**
+     * The window. Can be moved around. Probably breaks on web : (
+     */
+    public var window:Window;
+    public function scaleChar(char:String,amount:Float):Void {}
+    /**
+     * Swaps char out for char to. Handles creating it for you. 
+     * @param char 
+     * @param charTo 
+     */
+    public function swapChar(char:String, charTo:String):Void {}
+    /**
+     * Called at the start of the song.
+     * @param song Name of the song
+     */
+    dynamic public function start(song:String):Void {}
+    /**
+     * Called when a beat is hit
+     * @param beat Current Beat
+     */
+    dynamic public function beatHit(beat:Int):Void {}
+    /**
+     * Called every frame
+     * @param elapsed Time since last frame, used for correcting animation. Multiplying a number by this will make the thing "per second".
+     */
+    dynamic public function update(elapsed:Float):Void {}
+    /**
+     * Called when a step is hit, which is 1/4 of a beat. 
+     * @param step Current Step
+     */
+    dynamic public function stepHit(step:Int):Void {}
+    /**
+     * Called when the section switches to player 2. 
+     */
+    dynamic public function playerTwoTurn():Void {}
+    /**
+     * Called when player 2 misses a note. 
+     */
+    dynamic public function playerTwoMiss():Void {}
+    /**
+     * Called when player 2 hits a note. 
+     */
+    dynamic public function playerTwoSing():Void {}
+    /**
+     * Called when the section switches to player 1.
+     */
+    dynamic public function playerOneTurn():Void {}
+    /**
+     * Called when player 1 misses a note.
+     */
+    dynamic public function playerOneMiss():Void {}
+    /**
+     * Called when player 1 hits a note
+     */
+    dynamic public function playerOneSing():Void {}
+    /**
+     * Called when a note is hit
+     * @param player1 Whether player 1 hit the note
+     * @param note the note
+     * @param goodHit whether the note was hit well
+     */
+    dynamic public function noteHit(player1:Bool, note:Null<Note>, goodHit:Bool):Void {}
+} 
