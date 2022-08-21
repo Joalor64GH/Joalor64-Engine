d54cfed61a00a89f5c7d7fa3fd907bdc56e13e1d
Joalor64 <rhgarcon@gmail.com>
2022-08-21 08:26:47 -0400

    removing polymod support (potentially fix mods crash???)

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -100,20 +100,20 @@ class TitleState extends MusicBeatState
 		
 		//trace(path, FileSystem.exists(path));
 
-		#if (polymod)
-		if (sys.FileSystem.exists('polymods/')) {
+		/*#if (polymod && !html5)
+		if (sys.FileSystem.exists('mods/')) {
 			var folders:Array<String> = [];
-			for (file in sys.FileSystem.readDirectory('polymods/')) {
-				var path = haxe.io.Path.join(['polymods/', file]);
+			for (file in sys.FileSystem.readDirectory('mods/')) {
+				var path = haxe.io.Path.join(['mods/', file]);
 				if (sys.FileSystem.isDirectory(path)) {
 					folders.push(file);
 				}
 			}
 			if(folders.length > 0) {
-				polymod.Polymod.init({modRoot: "polymods", dirs: folders});
+				polymod.Polymod.init({modRoot: "mods", dirs: folders});
 			}
 		}
-		#end
+		#end*/
 		
 		#if CHECK_FOR_UPDATES
 		if(!closedState) {
