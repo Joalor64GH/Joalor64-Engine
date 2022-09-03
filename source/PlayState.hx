8649bb3549b6d2532b4daf5bd9ee0a76882204fd
Joalor64 <rhgarcon@gmail.com>
2022-09-03 17:30:55 -0400

    finally getting hxs scripts to work

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -850,6 +850,36 @@ class PlayState extends MusicBeatState
 		}
 		#end
 
+        #if HAXE_EXTENSION
+		var filesPushed:Array<String> = [];
+		var foldersToCheck:Array<String> = [Paths.getPreloadPath('haxescript/')];
+
+		#if MODS_ALLOWED
+		foldersToCheck.insert(0, Paths.mods('haxescript/'));
+		if(Paths.currentModDirectory != null && Paths.currentModDirectory.length > 0)
+			foldersToCheck.insert(0, Paths.mods(Paths.currentModDirectory + '/haxescript/'));
+		#end
+
+		for (folder in foldersToCheck)
+		{
+			if(FileSystem.exists(folder))
+			{
+				for (file in FileSystem.readDirectory(folder))
+				{
+					if(file.endsWith('.hxs') && !filesPushed.contains(file))
+					{
+						var expr = file;
+						var parser = new hscript.Parser();
+						var ast = parser.parseString(expr);
+						var interp = new hscript.Interp();
+						trace(interp.execute(ast));
+						filesPushed.push(file);
+					}
+				}
+			}
+		}
+		#end
+
 		#if HAXE_MODCHART
 		var filesPushed:Array<String> = [];
 		var foldersToCheck:Array<String> = [Paths.getPreloadPath('modcharts/')];
@@ -1237,6 +1267,36 @@ class PlayState extends MusicBeatState
 				for (file in FileSystem.readDirectory(folder))
 				{
 					if(file == '.hscript' && !filesPushed.contains(file))
+					{
+						var expr = file;
+						var parser = new hscript.Parser();
+						var ast = parser.parseString(expr);
+						var interp = new hscript.Interp();
+						trace(interp.execute(ast));
+						filesPushed.push(file);
+					}
+				}
+			}
+		}
+		#end
+
+		#if HAXE_EXTENSION
+		var filesPushed:Array<String> = [];
+		var foldersToCheck:Array<String> = [Paths.getPreloadPath('data/' + Paths.formatToSongPath(SONG.song) + '/')];
+
+		#if MODS_ALLOWED
+		foldersToCheck.insert(0, Paths.mods('data/' + Paths.formatToSongPath(SONG.song) + '/'));
+		if(Paths.currentModDirectory != null && Paths.currentModDirectory.length > 0)
+			foldersToCheck.insert(0, Paths.mods(Paths.currentModDirectory + '/data/' + Paths.formatToSongPath(SONG.song) + '/'));
+		#end
+
+		for (folder in foldersToCheck)
+		{
+			if(FileSystem.exists(folder))
+			{
+				for (file in FileSystem.readDirectory(folder))
+				{
+					if(file == '.hxs' && !filesPushed.contains(file))
 					{
 						var expr = file;
 						var parser = new hscript.Parser();
