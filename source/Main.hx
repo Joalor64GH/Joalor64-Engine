e8e16cd828221d40d0ac92794fdbae1c0bf10377
Joalor64 <rhgarcon@gmail.com>
2022-11-25 09:40:34 -0500

    i think this is the problem

--- source/Main.hx
+++ source/Main.hx
@@ -80,7 +80,7 @@ class Main extends Sprite
 		}
 	
 		ClientPrefs.loadDefaultKeys();
-		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
+		addChild(new FlxGame(gameWidth, gameHeight, initialState, framerate, framerate, skipSplash, startFullscreen));
 
 		fpsVar = new FPS(10, 3, 0xFFFFFF);
 		addChild(fpsVar);
